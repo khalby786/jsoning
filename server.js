7512f28c7ad2e7f86ea1c241313d2ce396bdda7a
Glitch (hello-express) <none>
2020-05-04 14:00:22 +0000

    🎀☂️ Checkpoint ./server.js:3168504/3150 ./package.json:3168504/282

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+// developer dependencies
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+const fs = require("fs");
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+module.exports = {
+  new: function(key) {
+    // fs.readFile("database.json", function(err, data) {
+    //   if (err) throw err;
+    //   const 
+    // })
+    const db = require('database.json');
+    console.log(db);
+    
+    // see if it exists
+    try {
+      
+    } catch {
+      
+    }
+    fs.appendFile(
+      "database.json",
+      `
+      ${key} {
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+      }
+      `,
+      function(err) {
+        if (err) throw err;
+        console.log("saved!");
+      }
+    );
+  }
+};
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+app.listen(process.env.PORT, function() {
+  console.log(`Listening carefully on port ${process.env.PORT}`);
 });
