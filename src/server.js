eb53a78bd1c638b548400276192594abcf240c50
khalby786 <khalby786@gmail.com>
2020-05-04 23:14:41 +0530

    perf: unnecessary fs module

--- src/server.js
+++ src/server.js
@@ -1,6 +1,3 @@
-const fs = require("fs");
-// var path = require('path');
-// var jsoning = require( path.resolve( __dirname, "./jsoning.js" ) );
 const jsoning = require(__dirname + '/jsoning.js');
 
 module.exports = jsoning;
