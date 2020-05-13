db69e085eda626c3a5d7ca12ebc3ca4488fa9ba8
khalby786 <khalby786@gmail.com>
2020-05-13 10:25:32 +0530

    vue pls don't do this

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  base: '/jsoning/',
+  title: 'Jsoning',
+  description: 'A simple key-value JSON-based persistent lightweight database.',
+  themeConfig: {
+    sidebarDepth: 3,
+    editLinks: true,
+    lastUpdated: true,
+  }
+};
