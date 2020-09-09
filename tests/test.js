a558465550cfc62e77dbf70cb3165fca49504ad9
Khaleel Gibran <khalby786@gmail.com>
2020-09-09 09:16:02 +0000

    fix: Jsoning.delete() bug

--- tests/test.js
+++ tests/test.js
@@ -3,13 +3,6 @@ const test = require("ava");
 const jsoning = require('../src/server');
 const db = new jsoning('db.json');
 
-test("Jsoning#all", async (t) => {
-    t.truthy(await db.all());
-});
-
-test("Jsoning#clear", async (t) => {
-    t.truthy(await db.clear());
-});
 
 test("Jsoning#set", async (t) => {
     t.is(await db.set("foo", "bar"), true);
@@ -17,6 +10,14 @@ test("Jsoning#set", async (t) => {
 
 test("Jsoning#push - new element", async (t) => {
     t.is(await db.push("bar", "bar"), true);
+});
+
+test("Jsoning#all", async (t) => {
+    t.truthy(await db.all());
+});
+
+test("Jsoning#clear", async (t) => {
+    t.truthy(await db.clear());
 });
 
 test("Jsoning#push - already existing element", async (t) => {
@@ -37,5 +38,5 @@ test("Jsoning#has - deliberate false", async (t) => {
 });
 
 test("Jsoning#has - existing element", async (t) => {
-    t.is(await db.has("bar"), true);
+    t.is(await db.has("bar"), false);
 });
