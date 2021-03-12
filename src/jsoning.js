8d17ef7f805341dcad95b4b523c3e83828a02bce
Khaleel Gibran <khalby786@gmail.com>
2021-03-12 18:32:28 +0400

    fix: prototype pollution vulnerability

--- src/jsoning.js
+++ src/jsoning.js
@@ -126,7 +126,7 @@ class Jsoning {
     let db = JSON.parse(
       fs.readFileSync(resolve(__dirname, this.database), "utf-8")
     );
-    if (db.hasOwnProperty(key)) {
+    if (Object.prototype.hasOwnProperty.call(db, key)) {
       try {
         const removeProp = key;
         const { [removeProp]: remove, ...rest } = db;
@@ -165,7 +165,7 @@ class Jsoning {
 
     let db = fs.readFileSync(resolve(__dirname, this.database), "utf-8");
     db = JSON.parse(db);
-    if (db[key]) {
+    if (Object.prototype.hasOwnProperty.call(db, key)) {
       let data = db[key];
       return data;
     } else {
@@ -245,7 +245,7 @@ class Jsoning {
     let db = JSON.parse(
       fs.readFileSync(resolve(__dirname, this.database), "utf-8")
     );
-    if (db[key]) {
+    if (Object.prototype.hasOwnProperty.call(db, key)) {
       // key exists
       let value = db[key];
       if (typeof value !== "number" || value === "") {
@@ -318,7 +318,7 @@ class Jsoning {
     let db = fs.readFileSync(resolve(__dirname, this.database), "utf-8");
     db = JSON.parse(db);
 
-    if (db.hasOwnProperty(key)) {
+    if (Object.prototype.hasOwnProperty.call(db, key)) {
       return true;
     } else {
       return false;
@@ -344,7 +344,7 @@ class Jsoning {
     let db = fs.readFileSync(resolve(__dirname, this.database), "utf-8");
     db = JSON.parse(db);
 
-    if (db.hasOwnProperty(key)) {
+    if (Object.prototype.hasOwnProperty.call(db, key)) {
       if (!Array.isArray(db[key])) {
         console.log(db);
         console.log(typeof db[key]);
